bf672389b71100d80f482a9ea882e75eaee91756
stevenporip <sp00032@mix.wvu.edu>
2024-02-25 19:07:32 -0500

    fixed commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <><h1>CS 230L</h1>
-    <h2>Section - 002</h2></>
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 002</h2>
+      <p>WVU ID: 800356927</p>
+      <p>Hi I am Steven Poripski</p>
+    </div>
   );
 }
 
